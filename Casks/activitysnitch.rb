d254c16a4778475521dced7c852c8ba6309fe5e9
Moritz Frenzel <moritz@noscito.network>
2026-09-05 15:57:43 +0200

    activitysnitch 1.0.0

--- Casks/activitysnitch.rb
+++ Casks/activitysnitch.rb
@@ -1,6 +1,6 @@
 cask "activitysnitch" do
   version "1.0.0"
-  sha256 "PLACEHOLDER"
+  sha256 "ea505aa9064c2d85193a2af901b960d14c7ab2a09af2202c5ff8a6bc285d516f"
 
   url "https://github.com/momorientes/activitysnitch/releases/download/v#{version}/ActivitySnitch-#{version}.zip"
   name "ActivitySnitch"
